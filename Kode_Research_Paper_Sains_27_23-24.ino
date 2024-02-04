eb3d74ec2f7b6ef74a285eaaa427d3ffc9a94609
NicolasThaddeusL <118431545+NicolasThaddeusL@users.noreply.github.com>
2024-02-05 06:38:42 +0700

    Mengubah nama Wi-Fi dan passwordnya

--- Kode_Research_Paper_Sains_27_23-24.ino
+++ Kode_Research_Paper_Sains_27_23-24.ino
@@ -1,23 +1,21 @@
-#include <LiquidCrystal_I2C.h> // Library for LCD
-#include "WiFiS3.h" //Library Wifi
-#include <ArduinoMqttClient.h> //Library MQTT PubSub
-#include <DFRobot_ENS160.h> //Library ENS160 / AQI Sensor
+#include <LiquidCrystal_I2C.h>
+#include "WiFiS3.h"
+#include <ArduinoMqttClient.h>
+#include <DFRobot_ENS160.h>
 
-#define I2C_COMMUNICATION  //I2C communication. Comment out this line of code if you want to use SPI communication.
- 
+#define I2C_COMMUNICATION
+
 #ifdef  I2C_COMMUNICATION
-  DFRobot_ENS160_I2C ENS160(&Wire, /*I2CAddr*/ 0x53);
+  DFRobot_ENS160_I2C ENS160(&Wire, 0x53);
 #else
   uint8_t csPin = D3;
   DFRobot_ENS160_SPI ENS160(&SPI, csPin);
 #endif
 
-LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4); //initialize LCD and address
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
  
-// char ssid[] = "CC Junior High School";      // your network SSID (name)
-char ssid[] = "Kolese Kanisius 4"
-// char pass[] = "Kanisius64";  // your network password
-char pass[] = "Kanisius192764"
+char ssid[] = "CC Junior High School";
+char pass[] = "Canisian64";
 int status = WL_IDLE_STATUS;
 char mqtt_user[] = ""; 
 char mqtt_pass[] = "";
@@ -28,9 +26,9 @@ int nilaiLdr = 0;
 
 float temp;
 float hum;
-const char broker[] = "broker.emqx.io";  //IP address of the EMQX broker.
+const char broker[] = "broker.emqx.io";
 int port = 1883;
-unsigned char bufferRTT[32] = {};  //serial receive data
+unsigned char bufferRTT[32] = {};
 char tempStr[15];
 char HumStr[15];
 char str[8];
@@ -41,7 +39,6 @@ unsigned int PMS1 = 0, PMS2_5 = 0, PMS10 = 0, FMHDS = 0, TPS = 0, HDS = 0, CR1 =
 char clientId[] = "LetsMakeThingsEverylthing";
 char topic_led[] = "mqtt/roomtest/lampu";
 char topic_ldr[] = "mqtt/roomtest/ldr";
-
 char topic_temp[] = "mqtt/roomtest/temperature";
 char topic_hum[] = "mqtt/roomtest/humidity";
 char topic_pm_satu[] = "mqtt/roomtest/pmsatu";
@@ -125,17 +122,17 @@ void setup()
   Serial1.begin(9600); 
   wifiConnect();
   lcd.begin(20,4);
-  lcd.init();//Initialize LCD1602 RGB display
+  lcd.init();
   lcd.backlight(); 
   
   while( NO_ERR != ENS160.begin() ){
-    Serial.println("Communication with device failed, please check connection");
+    Serial.println("Communication with ENS160 failed, please check the connection.");
     delay(3000);
   }
   
   Serial.println("Begin ok!");
   ENS160.setPWRMode(ENS160_STANDARD_MODE);
-  ENS160.setTempAndHum(/*temperature=*/25.0, /*humidity=*/50.0);
+  ENS160.setTempAndHum(25.0, 50.0);
   
   Serial.print("Attempting to connect to the MQTT broker.");
   if (!mqttClient.connect(broker, port)) {
@@ -159,8 +156,99 @@ void setup()
 void loop() {
   mqttClient.poll();
 
+  Serial.print("Temperatur : ");
+  sprintf(tempStr, "%d%d.%d", TPS / 100, (TPS / 10) % 10, TPS % 10);
+  Serial.print(tempStr);
+  Serial.println(" C");
+  mqttClient.beginMessage(topic_temp);
+  mqttClient.print(tempStr);
+  mqttClient.endMessage();
+
+  Serial.print("Humiditas   : ");
+  sprintf(tempStr, "%d%d.%d", HDS / 100, (HDS / 10) % 10, HDS % 10);
+  Serial.print(tempStr);
+  Serial.println(" %");
+  mqttClient.beginMessage(topic_hum);
+  mqttClient.print(tempStr);
+  mqttClient.endMessage();
+
+  Serial.print("PM 1.0: ");
+  Serial.print(PMS1);
+  Serial.println(" ug/m3");
+  mqttClient.beginMessage(topic_pm_satu);
+  mqttClient.print(PMS1);
+  mqttClient.endMessage();
+
+  Serial.print("PM 2.5: ");
+  Serial.print(PMS2_5);
+  Serial.println(" ug/m3");
+  mqttClient.beginMessage(topic_pm_dua_lima);
+  mqttClient.print(PMS2_5);
+  mqttClient.endMessage();
+
+  Serial.print("PM 10: ");
+  Serial.print(PMS10);
+  Serial.println(" ug/m3");
+  mqttClient.beginMessage(topic_pm_sepuluh);
+  mqttClient.print(PMS10);
+  mqttClient.endMessage();
+
+  uint8_t Status = ENS160.getENS160Status();
+  Serial.print("Status pengoperasian sensor : ");
+  Serial.println(Status);
+  mqttClient.beginMessage(topic_status);
+  mqttClient.print(Status);
+  mqttClient.endMessage();
+  
+  uint8_t AQI = ENS160.getAQI();
+  Serial.print("Air Quality Index : ");
+  Serial.println(AQI);
+  lcd.setCursor(11, 2);
+  lcd.print(AQI);
+  mqttClient.beginMessage(topic_AQI);
+  mqttClient.print(AQI);
+  mqttClient.endMessage();
+  
+  uint16_t TVOC = ENS160.getTVOC();
+  Serial.print("Konsentrasi Total Senyawa Organik yang Mudah Menguap (TVOC) : ");
+  Serial.print(TVOC);
+  Serial.println(" ppb");
+  mqttClient.beginMessage(topic_TVOC);
+  mqttClient.print(TVOC);
+  mqttClient.endMessage();
+  
+  uint16_t ECO2 = ENS160.getECO2();
+  Serial.print("Konsentrasi Karbon Dioksida Setara (eCO2) : ");
+  Serial.print(ECO2);
+  Serial.println(" ppm");
+  mqttClient.beginMessage(topic_eCO2);
+  mqttClient.print(ECO2);
+  mqttClient.endMessage();
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("eCO2:");
+  lcd.setCursor(7, 0);
+  lcd.print(ECO2);
+  lcd.setCursor(12, 0);
+  lcd.print("ppm");
+  lcd.setCursor(0, 1); 
+  lcd.print("TVOC:");
+  lcd.setCursor(7, 1);
+  lcd.print(TVOC);
+  lcd.setCursor(12, 1);
+  lcd.print("ppb");
+  lcd.setCursor(0, 2); 
+  lcd.print("AQI Index:");
+  lcd.setCursor(11,2);
+  lcd.print(AQI);
+  lcd.setCursor(0, 3);
+  lcd.print("PM2.5:");
+  lcd.setCursor(7,3);
+  lcd.print(PMS2_5);
+
   while (!Serial1.available());
-  while (Serial1.available() > 0)  //Check whether there is any serial data
+  while (Serial1.available() > 0)
   {
     for (int i = 0; i < 32; i++) {
       col = Serial1.read();
@@ -174,27 +262,27 @@ void loop() {
     CR2 = 0;
     for (int i = 0; i < 30; i++)
       CR2 += bufferRTT[i];
-    if (CR1 == CR2)  //Check
+    if (CR1 == CR2)
     {
-      PMSa1 = bufferRTT[10];        //Read PM1 high 8-bit data
-      PMSb1 = bufferRTT[11];        //Read PM1 low 8-bit data
-      PMS1 = (PMSa1 << 8) + PMSb1;  //PM1 data
+      PMSa1 = bufferRTT[10];
+      PMSb1 = bufferRTT[11]; 
+      PMS1 = (PMSa1 << 8) + PMSb1;
 
-      PMSa2_5 = bufferRTT[12];            //Read PM2.5 high 8-bit data
-      PMSb2_5 = bufferRTT[13];            //Read PM2.5 low 8-bit data
-      PMS2_5 = (PMSa2_5 << 8) + PMSb2_5;  //PM2.5 data
+      PMSa2_5 = bufferRTT[12];
+      PMSb2_5 = bufferRTT[13];
+      PMS2_5 = (PMSa2_5 << 8) + PMSb2_5;
 
-      PMSa10 = bufferRTT[14];          //Read PM10 high 8-bit data
-      PMSb10 = bufferRTT[15];          //Read PM10 low 8-bit data
-      PMS10 = (PMSa10 << 8) + PMSb10;  //PM10 data
+      PMSa10 = bufferRTT[14];
+      PMSb10 = bufferRTT[15];
+      PMS10 = (PMSa10 << 8) + PMSb10;
 
-      TPSa = bufferRTT[24];      //Read temperature high 8-bit data
-      TPSb = bufferRTT[25];      //Read temperature low 8-bit data
-      TPS = (TPSa << 8) + TPSb;  //Temperature data
+      TPSa = bufferRTT[24];
+      TPSb = bufferRTT[25];
+      TPS = (TPSa << 8) + TPSb;
 
-      HDSa = bufferRTT[26];      //Read humidity high 8-bit data
-      HDSb = bufferRTT[27];      //Read humidity low 8-bit data
-      HDS = (HDSa << 8) + HDSb;  //Humidity data
+      HDSa = bufferRTT[26];
+      HDSb = bufferRTT[27];
+      HDS = (HDSa << 8) + HDSb;
     } else {
       PMS1 = 0;
       PMS2_5 = 0;
@@ -204,104 +292,9 @@ void loop() {
     }
   }
 
-  Serial.print("Temp : ");
-  sprintf(tempStr, "%d%d.%d", TPS / 100, (TPS / 10) % 10, TPS % 10);
-  Serial.print(tempStr);
-  Serial.println(" C");  //Display temperature
-  mqttClient.beginMessage(topic_temp);
-  mqttClient.print(tempStr);
-  mqttClient.endMessage();
-
-  Serial.print("RH   : ");
-  sprintf(tempStr, "%d%d.%d", HDS / 100, (HDS / 10) % 10, HDS % 10);
-  Serial.print(tempStr);  //Display humidity
-  Serial.println(" %");   //%
-  mqttClient.beginMessage(topic_hum);
-  mqttClient.print(tempStr);
-  mqttClient.endMessage();
-
-  Serial.print("PM1.0: ");
-  Serial.print(PMS1);
-  Serial.println(" ug/m3");  //Display PM1.0 unit  ug/m³
-  mqttClient.beginMessage(topic_pm_satu);
-  mqttClient.print(PMS1);
-  mqttClient.endMessage();
-
-  lcd.setCursor(0, 3);
-  lcd.print("PMS2.5:");
-  Serial.print("PM 2.5: ");
-  Serial.print(PMS2_5);
-  Serial.println(" ug/m3");  //Display PM2.5 unit  ug/m³
-  lcd.setCursor(10, 3);
-  lcd.print(PMS2_5);
-  mqttClient.beginMessage(topic_pm_dua_lima);
-  mqttClient.print(PMS2_5);
-  mqttClient.endMessage();
-
-  Serial.print("PM 10: ");
-  Serial.print(PMS10);
-  Serial.println(" ug/m3");  //Display PM 10 unit  ug/m³
-  mqttClient.beginMessage(topic_pm_sepuluh);
-  mqttClient.print(PMS10);
-  mqttClient.endMessage();
-
   mqttClient.beginMessage(topic_ldr);
   mqttClient.print(random(1000));
   mqttClient.endMessage();
-  
-  uint8_t Status = ENS160.getENS160Status();
-  Serial.print("Sensor operating status : ");
-  Serial.println(Status);
-  mqttClient.beginMessage(topic_status);
-  mqttClient.print(Status);
-  mqttClient.endMessage();
-  
-  uint8_t AQI = ENS160.getAQI();
-  Serial.print("Air quality index : ");
-  Serial.println(AQI);
-  lcd.setCursor(11, 2);
-  lcd.print(AQI);
-  mqttClient.beginMessage(topic_AQI);
-  mqttClient.print(AQI);
-  mqttClient.endMessage();
-  
-  uint16_t TVOC = ENS160.getTVOC();
-  Serial.print("Concentration of total volatile organic compounds : ");
-  Serial.print(TVOC);
-  Serial.println(" ppb");
-  mqttClient.beginMessage(topic_TVOC);
-  mqttClient.print(TVOC);
-  mqttClient.endMessage();
-  
-  uint16_t ECO2 = ENS160.getECO2();
-  Serial.print("Carbon dioxide equivalent concentration : ");
-  Serial.print(ECO2);
-  Serial.println(" ppm");
-  mqttClient.beginMessage(topic_eCO2);
-  mqttClient.print(ECO2);
-  mqttClient.endMessage();
-
-  lcd.clear();
-  lcd.setCursor(0, 0); //Set displaying from (0, 0)
-  lcd.print("eCO2:"); //Display eCO2
-  lcd.setCursor(7, 0);
-  lcd.print(ECO2);
-  lcd.setCursor(12, 0);
-  lcd.print("ppm");
-  lcd.setCursor(0, 1); 
-  lcd.print("TVOC:"); //Display TVOC
-  lcd.setCursor(7, 1);
-  lcd.print(TVOC);
-  lcd.setCursor(12, 1);
-  lcd.print("ppb");
-  lcd.setCursor(0, 2); 
-  lcd.print("AQI Index:"); //Display AQI
-  lcd.setCursor(11,2);
-  lcd.print(AQI);
-  lcd.setCursor(0, 3);
-  lcd.print("PM2.5:");
-  lcd.setCursor(7,3);
-  lcd.print(PMS2_5);
   
   delay(10000);
   lcd.clear();
